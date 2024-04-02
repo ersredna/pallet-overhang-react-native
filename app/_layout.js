fc638875a5c14330ae29c3138a0d2f72ba6aced5
Ersredna <ersredna@gmail.com>
2024-04-02 15:51:25 -0700

    initialized repo

---
+++ app/_layout.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Home = () => {
+    return (
+        <View>
+            <Text>testestestestestestestsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsetsettestestestestestestestestetestestestestestestestestestestestestestestestestestestessetsetsetsetsetsetsetsettestestestestestestestestes</Text>
+        </View>
+    )
+}
+
+export default Home
